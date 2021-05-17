b305df5d032d2710625149ad97281fe1d41ed7e6
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-05-17 16:50:15 -0400

    set up multiple rooms

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html');
+});
+
 // tech namespace
 const tech = io.of('/tech');
 
