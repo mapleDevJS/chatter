d11090feae82b4cb0d6b8a16c336b8cb5d060273
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-07-23 12:39:10 -0400

    Update nodemon and other dependencies
    Upgraded nodemon to version 3.1.4 and updated multiple dependencies to their newer versions. Removed unnecessary dependencies and updated necessary engines for node to keep the compatibility. Improved the overall package-lock file by removing excess and unused packages.

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+import express from 'express';
+import http from 'http';
+import { Server as socketIOServer } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new socketIOServer(server);
 const port = 3000;
+
+const publicFileResponse = (file) => (req, res) => {
+    res.sendFile(`${__dirname}/public/${file}.html`);
+};
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.get('/', publicFileResponse('index'));
+app.get('/javascript', publicFileResponse('javascript'));
+app.get('/swift', publicFileResponse('swift'));
+app.get('/css', publicFileResponse('css'));
 
-app.get('/javascript', (req, res) => {
-    res.sendFile(__dirname + '/public/javascript.html');
-});
-
-app.get('/swift', (req, res) => {
-    res.sendFile(__dirname + '/public/swift.html');
-});
-
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/css.html');
-});
-
-// tech namespace
 const tech = io.of('/tech');
-
 tech.on('connection', (socket) => {
     socket.on('join', (data) => {
         socket.join(data.room);
         tech.in(data.room).emit('message', `New user joined ${data.room} room!`);
-    })
-
+    });
     socket.on('message', (data) => {
         console.log(`message: ${data.msg}`);
         tech.in(data.room).emit('message', data.msg);
     });
-
     socket.on('disconnect', () => {
         console.log('user disconnected');
-
         tech.emit('message', 'user disconnected');
-    })
-})
+    });
+});
